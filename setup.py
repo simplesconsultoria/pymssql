simplesconsultoria/pymssql

commit 3f1f1b31341bbb4168e9fcbdeac5f506946a6f80

Remove usage of custom build_ext class part 1: Unix/Linux.
Fixes issue #90.

setup.py
@@ -39,7 +39,7 @@ except ImportError:
 from distutils import log
 from distutils.cmd import Command
 from distutils.command.clean import clean as _clean
-from Cython.Distutils import build_ext as _build_ext
+from Cython.Distutils import build_ext
 import struct
 
 _extra_compile_args = [
@@ -87,10 +87,10 @@ if sys.platform == 'darwin':
         '/opt/local/freetds/lib'
     ]
 
-class build_ext(_build_ext):
+# TODO: Still need to remove reliance on this class for win32
+class deprecated_build_ext(build_ext):
     """
-    Subclass the Cython build_ext command so it extracts freetds.zip if it
-    hasn't already been done.
+    Subclass the Cython build_ext command.
     """
 
     def build_extensions(self):
@@ -149,21 +149,11 @@ class clean(_clean):
         for ext in self.distribution.ext_modules:
             cy_sources = [s for s in ext.sources if s.endswith('.pyx')]
             for cy_source in cy_sources:
-                c_source = cy_source[:-3] + 'c'
-                if osp.exists(c_source):
-                    log.info('removing %s', c_source)
-                    os.remove(c_source)
                 so_built = cy_source[:-3] + 'so'
                 if osp.exists(so_built):
                     log.info('removing %s', so_built)
                     os.remove(so_built)
 
-        # Check if we need to remove the freetds directory
-        if WINDOWS:
-            # If the directory exists, remove it
-            if osp.isdir(FREETDS):
-                import shutil
-                shutil.rmtree(FREETDS)
 
 class release(Command):
     """
@@ -286,17 +276,18 @@ setup(
         ('', ['_mssql.pyx', 'pymssql.pyx'])
     ],
     zip_safe = False,
-    setup_requires=["Cython>=0.15.1"],
     ext_modules = [
-        Extension('_mssql', ['_mssql.pyx'],
+        Extension('_mssql', ['_mssql.c'],
             extra_compile_args = _extra_compile_args,
             include_dirs = include_dirs,
-            library_dirs = library_dirs
+            library_dirs = library_dirs,
+            libraries = libraries,
         ),
-        Extension('pymssql', ['pymssql.pyx'],
+        Extension('pymssql', ['pymssql.c'],
             extra_compile_args = _extra_compile_args,
             include_dirs = include_dirs,
-            library_dirs = library_dirs
+            library_dirs = library_dirs,
+            libraries = libraries,
         ),
     ],
 

.gitignore
@@ -1,7 +1,6 @@
 build/
 dist/
 tests/tmp/
-*.c
 *.egg-info*
 *komodo*
 *.pyc

MANIFEST.in
@@ -2,7 +2,6 @@ include ez_setup.py
 include ChangeLog
 include LICENSE
 include *.pxd
-include win32/freetds.zip
 recursive-include .pyrex *
 recursive-include nagios-plugin *
 recursive-include tests *.py

commit 54e9ff18fbd77e871e9e090a207b7cc43a5dd33c

Fix clean, I had broken it in 3f1f1b31341bbb4168e9fcbdeac5f506946a6f80

setup.py
@@ -147,9 +147,9 @@ class clean(_clean):
     def run(self):
         _clean.run(self)
         for ext in self.distribution.ext_modules:
-            cy_sources = [s for s in ext.sources if s.endswith('.pyx')]
+            cy_sources = [s for s in ext.sources if s.endswith('.c')]
             for cy_source in cy_sources:
-                so_built = cy_source[:-3] + 'so'
+                so_built = cy_source[:-1] + 'so'
                 if osp.exists(so_built):
                     log.info('removing %s', so_built)
                     os.remove(so_built)